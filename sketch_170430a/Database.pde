a91b03d65053330c1bc9c9702697c838ca024b65
Cole Christini <cole.christini@gmail.com>
2017-05-01 20:51:02 -0400

    Got networking fixed
    Packet sniffing broke though

--- sketch_170430a/Database.pde
+++ sketch_170430a/Database.pde
@@ -1,14 +1,10 @@
 import processing.net.*;
 import org.rsg.carnivore.*;
 import org.rsg.lib.Log;
-boolean trigger=false;
-
+ boolean trigger=false;
 class Database {
-
-Client c;
-String ip;
-int port;
-String hostName;
+  String username;
+  Client c;
   boolean isExisting;
   int nullvar=000;
   int length;
@@ -17,16 +13,13 @@ String hostName;
   String[] merge=new String[3];
   String[][] users = new String[length][3];
   String[] getUser= new String[3];
+  boolean userFound;
+int user=0;
+PApplet _myApplet;
 
-
-
-  Database(int arraylength, String[][]usersinternal,String ipToUse,int portToUse,String host) {
-    c = new Client(this, ip, port);  // Connect to server on port
-
-ip=ipToUse;
-port=portToUse;
-hostName=host;
+  Database(int arraylength, String[][]usersinternal, PApplet myApplet) {
     arraylength=length;
+    _myApplet=myApplet;
     usersinternal= new String[arraylength][3];
     for (int i=0; i<length; i++) {
       for (int x=0; x<2; x++) {
@@ -74,60 +67,81 @@ hostName=host;
   } // deleteuser
 
 
-  void networkStart(String ip,int port , String hostName) {
+  void networkStart(String ip, int port, String hostname) {
+    c = new Client(_myApplet, ip, port);  // Connect to server on port
 
-    c.write(HostName); // Be polite and say who we are
+    c.write(hostname); // Be polite and say who we are
   }   // networkStart
 
 
   void networkRead() {
-    if (c.readString=="insertUser") {//send if you want to have user inserted
+
+    if (c.readString()=="insertUser") {//send if you want to have user inserted
       getUser[1]=c.readString();//username
-      for (int i=0; i<length; i++) {
-        if (c.readString()==users[i[1]]) {
+      for (int i=0; i<users.length; i++) {
+        if (c.readString()==users[i][1]) {
           isExisting=true;
-        }
-        else{
+        } else {
           isExisting=false;
         }
-      delay(5);
-      if(isExisting==false){
-      getUser[2]=c.readString();//password
-      delay(5);
-      getUser[3].readString();//interests
-      database.insert(getUser);//insert
-      c.write("done");
-    }}
-    if (c.readString=="login") {
-      int user;
-
-
-          user=i;
-          c.write("user found");
+        delay(5);
+        if (isExisting==false) {
+          getUser[2]=c.readString();//password
           delay(5);
+          getUser[3]=c.readString();//interests
+          getUser = new String[3];
+          for (int v=0; v<2; v++) {
+            users[length-1][v]=getUser[v];
+          }
+          for (int q=0; q<length; q++) {
+            temp[q]=users[q][0]+insert+users[q][1]+insert+users[q][2];
+            sort(temp);
+          }
+          for (int z=0; z<temp.length; z++) {
+            users[i]=split(temp[i], ";");
+          }//insert
+          c.write("done");
         }
       }
-      if (c.readString()==users[user][2]) {
-        c.write("correct password");
-      } else {
-        c.write("wrong password");
+      if (c.readString()=="login") {
+        username=c.readString();
+
+        for (int l=0; l<users.length; l++) {
+          if (username==users[l][1]) {
+            user=l;
+            c.write("user found");
+            delay(5);
+            userFound=true;
+          }
+        }
       }
-
+      if (userFound==true) {
+        if (c.readString()==users[user][2]) {
+          c.write("correct password");
+        } else {
+          c.write("wrong password");
+        }
+      }
+    }
   } // networkRead
-void debug(int checkTimes){
-  CarnivoreP5 c2;
-  c2 = new CarnivoreP5(this);
+  void debug(int checkTimes){
+   CarnivoreP5 c2;
+   c2 = new CarnivoreP5(this);
 
 
-if(c.readString=="ping"){
-c.write("Got ping"); //c;
-}
-if(trigger==true){
-  println("(" + p.strTransportProtocol + " packet) " + p.senderSocket() + " > " + p.receiverSocket());
-}
-}
+   if(c.readString()=="ping"){
+   c.write("Got ping"); //c;
+   }
+   if(trigger==true){
+   println("(" + p.strTransportProtocol + " packet) " + p.senderSocket() + " > " + p.receiverSocket());
+   }
+   }
 } // class Database
 
-void packetEvent(CarnivorePacket p){
-trigger=true;
+void setup() {
+  
 }
+
+void packetEvent(CarnivorePacket c){
+ trigger=true;
+ }
--- sketch_170430a/build/source/sketch_170430a.java
+++ sketch_170430a/build/source/sketch_170430a.java
@@ -137,14 +137,10 @@ class Database {
 
 
 
-boolean trigger=false;
-
+ boolean trigger=false;
 class Database {
-
-Client c;
-String ip;
-int port;
-String hostName;
+  String username;
+  Client c;
   boolean isExisting;
   int nullvar=000;
   int length;
@@ -153,16 +149,13 @@ String hostName;
   String[] merge=new String[3];
   String[][] users = new String[length][3];
   String[] getUser= new String[3];
+  boolean userFound;
+int user=0;
+PApplet _myApplet;
 
-
-
-  Database(int arraylength, String[][]usersinternal,String ipToUse,int portToUse,String host) {
-    c = new Client(this, ip, port);  // Connect to server on port
-
-ip=ipToUse;
-port=portToUse;
-hostName=host;
+  Database(int arraylength, String[][]usersinternal, PApplet myApplet) {
     arraylength=length;
+    _myApplet=myApplet;
     usersinternal= new String[arraylength][3];
     for (int i=0; i<length; i++) {
       for (int x=0; x<2; x++) {
@@ -210,63 +203,84 @@ hostName=host;
   } // deleteuser
 
 
-  public void networkStart(String ip,int port , String hostName) {
+  public void networkStart(String ip, int port, String hostname) {
+    c = new Client(_myApplet, ip, port);  // Connect to server on port
 
-    c.write(HostName); // Be polite and say who we are
+    c.write(hostname); // Be polite and say who we are
   }   // networkStart
 
 
   public void networkRead() {
-    if (c.readString=="insertUser") {//send if you want to have user inserted
+
+    if (c.readString()=="insertUser") {//send if you want to have user inserted
       getUser[1]=c.readString();//username
-      for (int i=0; i<length; i++) {
-        if (c.readString()==users[i[1]]) {
+      for (int i=0; i<users.length; i++) {
+        if (c.readString()==users[i][1]) {
           isExisting=true;
-        }
-        else{
+        } else {
           isExisting=false;
         }
-      delay(5);
-      if(isExisting==false){
-      getUser[2]=c.readString();//password
-      delay(5);
-      getUser[3].readString();//interests
-      database.insert(getUser);//insert
-      c.write("done");
-    }}
-    if (c.readString=="login") {
-      int user;
-
-
-          user=i;
-          c.write("user found");
+        delay(5);
+        if (isExisting==false) {
+          getUser[2]=c.readString();//password
           delay(5);
+          getUser[3]=c.readString();//interests
+          getUser = new String[3];
+          for (int v=0; v<2; v++) {
+            users[length-1][v]=getUser[v];
+          }
+          for (int q=0; q<length; q++) {
+            temp[q]=users[q][0]+insert+users[q][1]+insert+users[q][2];
+            sort(temp);
+          }
+          for (int z=0; z<temp.length; z++) {
+            users[i]=split(temp[i], ";");
+          }//insert
+          c.write("done");
         }
       }
-      if (c.readString()==users[user][2]) {
-        c.write("correct password");
-      } else {
-        c.write("wrong password");
+      if (c.readString()=="login") {
+        username=c.readString();
+
+        for (int l=0; l<users.length; l++) {
+          if (username==users[l][1]) {
+            user=l;
+            c.write("user found");
+            delay(5);
+            userFound=true;
+          }
+        }
       }
-
+      if (userFound==true) {
+        if (c.readString()==users[user][2]) {
+          c.write("correct password");
+        } else {
+          c.write("wrong password");
+        }
+      }
+    }
   } // networkRead
-public void debug(int checkTimes){
-  CarnivoreP5 c2;
-  c2 = new CarnivoreP5(this);
+  public void debug(int checkTimes){
+   CarnivoreP5 c2;
+   c2 = new CarnivoreP5(this);
 
 
-if(c.readString=="ping"){
-c.write("Got ping"); //c;
-}
-if(trigger==true){
-  println("(" + p.strTransportProtocol + " packet) " + p.senderSocket() + " > " + p.receiverSocket());
-}
-}
+   if(c.readString()=="ping"){
+   c.write("Got ping"); //c;
+   }
+   if(trigger==true){
+   println("(" + p.strTransportProtocol + " packet) " + p.senderSocket() + " > " + p.receiverSocket());
+   }
+   }
 } // class Database
 
-public void packetEvent(CarnivorePacket p){
-trigger=true;
+public void setup() {
+   Client jokeClient = new Client(this, "hi-there", 123);
 }
+
+public void packetEvent(CarnivorePacket c){
+ trigger=true;
+ }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "sketch_170430a" };
     if (passedArgs != null) {
